052b1b00b2176cea5fc12f9e2988d951b018c992
Navya <125690352+NavyaaReddyy@users.noreply.github.com>
2025-02-27 19:09:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Your News API key here
-const API_KEY = 'your_api_key';
+const API_KEY = '1b5c5a3403e06a8e06c7548a95c1feb2';
 
 // Coordinates for major countries (you can add more countries as needed)
 const countryCoordinates = {
